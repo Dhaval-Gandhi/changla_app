b01d9f352ca83d9c0fcdaf020ddd4923a3353fac
Dhaval G <dhavalg.spaceo@gmail.com>
2022-06-17 17:27:25 +0530

    password change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with :name => "admin", :password => "admin" 
+  http_basic_authenticate_with :name => "changla", :password => "9993" 
 end
